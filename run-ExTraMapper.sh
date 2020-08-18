5121f1cb2431f127e6cd277e4917fa08181d7be0
ay-lab <ferhatay@lji.org>
2020-08-18 15:24:05 -0700

    Update run-ExTraMapper.sh

--- run-ExTraMapper.sh
+++ run-ExTraMapper.sh
@@ -4,11 +4,9 @@ set -o errexit
 
 # read all the configuration parameters first
 source config.conf
-
 export EXTRAMAPPER_DIR=$EXTRAMAPPERDIR
 
-#step=$1
-
+#define the variables
 dataDir=${EXTRAMAPPER_DIR}/preprocess/data
 dataDirPerPair=${EXTRAMAPPER_DIR}/preprocess/data/$org1-$org2
 referenceGenomesDir=$dataDir/reference_genomes
@@ -20,7 +18,6 @@ GTFsummaryDir=$dataDirPerPair/GTFsummaries
 perGenePairPickleDir=$dataDirPerPair/perGenePairPickledInfo
 liftOverFilesDir=$dataDirPerPair/liftoverRelatedFiles
 perExonLiftoverDir=$dataDirPerPair/perExonLiftoverCoords
-# 
 
 # Some example input gene pairs #
 genePairId=ENSG00000078900-ENSMUSG00000029026 # TP73
@@ -29,12 +26,10 @@ genePairId=ENSG00000157764-ENSMUSG00000002413 # BRAF
 genePairId=ENSG00000143315-ENSMUSG00000050229 # 1 exon gene pair
 genePairId=ENSG00000140416-ENSMUSG00000032366 # TPM1 vs Tpm1 
 genePairId=ENSG00000141510-ENSMUSG00000059552 # TP53
+#genePairId=$1
 
-#genePairId=ENSG00000063127-ENSMUSG00000070568 # first problem
+#define the liftover minimum match threshold
+MAPPED_EXON_THRES=1.0
 
-genePairId=ENSG00000124299-ENSMUSG00000063931
-
-genePairId=ENSG00000063127-ENSMUSG00000070568
-
-MAPPED_EXON_THRES=0.9
+#run ExTramapper (python 2.7 tested)
 python ${EXTRAMAPPER_DIR}/scripts/ExTraMapper.py ${MAPPED_EXON_THRES} $org1 $org2 $genePairId > allOutputs.txt
