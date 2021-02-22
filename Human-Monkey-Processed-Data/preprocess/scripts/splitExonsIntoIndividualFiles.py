6b38ef2b9b1ece50a9165c26abcd15b859c80c6f
ay-lab <ferhatay@lji.org>
2021-02-22 15:39:57 -0800

    Add files via upload

---
+++ Human-Monkey-Processed-Data/preprocess/scripts/splitExonsIntoIndividualFiles.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import sys
+
+def main(argv):
+	infilename=argv[1]
+	outdir=argv[2]
+	whichCol=int(argv[3])-1
+	fileSuffix=argv[4]
+	infile=open(infilename,'r')
+	lastExon="dummy"
+	outfile=open("dummy.txt",'w')
+	for line in infile:
+		newExon=line.rstrip().split()[whichCol] # where exon name is
+		if newExon!=lastExon:
+			outfile.close()
+			outfile=open(outdir+"/"+newExon+fileSuffix,'w')
+		#
+		outfile.write(line)
+		lastExon=newExon
+	#
+	outfile.close()
+	return
+
+if __name__ == "__main__":
+	main(sys.argv)
+#
+
